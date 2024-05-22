9bdf59067b4a0661c1f670c48f2dc5bfe70981d1
alejandropj <alejandroparrajimenez@gmail.com>
2024-05-22 12:56:41 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleAWSSecrets.Helpers;
+using ConsoleAWSSecrets.Models;
+using Newtonsoft.Json;
+
+Console.WriteLine("Ejemplo secrets manager");
+string miSecreto = await HelperSecretManager.GetSecretAsync();
+Console.WriteLine(miSecreto);
+
+KeysModel model = JsonConvert.DeserializeObject<KeysModel>(miSecreto);
+Console.WriteLine("MySQL: " + model.Mysql);
+Console.WriteLine("MySQL: " + model.Mysql);
+Console.WriteLine("MySQL: " + model.Mysql);
