62c6a55c2d954deeff7c171efd103230c7204f9b
alejandropj <alejandroparrajimenez@gmail.com>
2024-05-23 09:56:05 +0200

    23/05

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConsoleAWSSecrets;
 using ConsoleAWSSecrets.Helpers;
 using ConsoleAWSSecrets.Models;
+using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 
 Console.WriteLine("Ejemplo secrets manager");
@@ -9,5 +11,22 @@ Console.WriteLine(miSecreto);
 
 KeysModel model = JsonConvert.DeserializeObject<KeysModel>(miSecreto);
 Console.WriteLine("MySQL: " + model.Mysql);
-Console.WriteLine("MySQL: " + model.Mysql);
-Console.WriteLine("MySQL: " + model.Mysql);
+
+//Almacenamiento del objeto KeysModel dentro de DY
+var provider =
+    new ServiceCollection()
+    .AddTransient<ClaseTest>()
+    .AddSingleton<KeysModel>(x => model)
+    .BuildServiceProvider();
+
+//En cualquier clase podemos recuperar las keys,
+//En el propio program si necesitamos connectionstring o en 
+//cualquier otra clase de inyeccion
+
+var service = provider.GetService<KeysModel>();
+string connectionString = service.Mysql;
+Console.WriteLine(connectionString);
+
+
+var test = provider.GetService<ClaseTest>();
+Console.WriteLine("Api: " + test.GetValue());
